b544ae914fc68782e4a75d32d076ae22d8df93b0
Tobias Sunderdiek <sunderdiek@gmail.com>
2023-04-07 22:55:02 +0200

    add test case

--- project_3_deploy_model_with_fastapi/starter/main.py
+++ project_3_deploy_model_with_fastapi/starter/main.py
@@ -4,14 +4,14 @@ from starter.ml import model
 from joblib import load
 
 app = FastAPI()
-lr_model = load('data/lr_model.joblib')
+lr_model = load('starter/data/lr_model.joblib')
 
 
 @app.get("/")
-def root():
+async def root():
     return "API to predict if salary is greater or lower 50k based on census data"
 
 
 @app.post("/predict_salary")
-def predict_salary(person: Person):
-    model.inference(lr_model, person)
+async def predict_salary(person: Person):
+    return model.inference(lr_model, person)
