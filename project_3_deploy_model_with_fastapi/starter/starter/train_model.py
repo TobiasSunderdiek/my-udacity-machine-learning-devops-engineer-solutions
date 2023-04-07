012e35fe9aa8458f0808fb98960188714e2fab86
Tobias Sunderdiek <sunderdiek@gmail.com>
2023-04-07 22:36:16 +0200

    add model file

--- project_3_deploy_model_with_fastapi/starter/starter/train_model.py
+++ project_3_deploy_model_with_fastapi/starter/starter/train_model.py
@@ -2,13 +2,13 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
-from model import train_model
+from ml.model import train_model
 from joblib import dump
 
-data = pd.read_csv('../../data/census.csv')
+data = pd.read_csv('starter/data/census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, stratify=data['salary'])
 
 cat_features = [
     "workclass",
@@ -28,5 +28,9 @@ X_test, y_test, _, _ = process_data(
     test, categorical_features=cat_features, label='salary', training=False, encoder=encoder, lb=lb)
 
 # Train and save a model.
-lr_model = train_model(X_train, y_train)
-dump(lr_model, '/../data/lr_model.joblib')
+def train_model():
+    lr_model = train_model(X_train, y_train)
+    dump(lr_model, 'starter/data/lr_model.joblib')
+
+if __name__ == '__main__':
+    train_model()
