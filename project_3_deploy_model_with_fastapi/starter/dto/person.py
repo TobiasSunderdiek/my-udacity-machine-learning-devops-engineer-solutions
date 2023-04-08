68bff4ac1f8da787eb450555a1457ee9d9e45672
Tobias Sunderdiek <sunderdiek@gmail.com>
2023-04-08 13:29:49 +0200

    fix hyphens

--- project_3_deploy_model_with_fastapi/starter/dto/person.py
+++ project_3_deploy_model_with_fastapi/starter/dto/person.py
@@ -7,12 +7,12 @@ class Person(BaseModel):
     fnlgt: int
     education: str
     education_num: int = Field(..., alias='education-num')
-    marital_status: str = Field(..., alias='marital_status')
+    marital_status: str = Field(..., alias='marital-status')
     occupation: str
     relationship: str
     race: str
     sex: str
     capital_gain: int = Field(..., alias='capital-gain')
     capital_loss: int = Field(..., alias='capital-loss')
-    hours_per_week: int = Field(..., alias='hours_per_week')
+    hours_per_week: int = Field(..., alias='hours-per-week')
     native_country: str = Field(..., alias='native-country')
