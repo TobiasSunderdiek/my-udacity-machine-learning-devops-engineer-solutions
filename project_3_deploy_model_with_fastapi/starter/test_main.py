b2e0863ac3b8fe486cf88719b5c640778d135734
Tobias Sunderdiek <sunderdiek@gmail.com>
2023-04-08 13:20:03 +0200

    add test person

--- project_3_deploy_model_with_fastapi/starter/test_main.py
+++ project_3_deploy_model_with_fastapi/starter/test_main.py
@@ -10,27 +10,41 @@ def test_welcome_msg():
   assert resp.json() == 'API to predict if salary is greater or lower 50k based on census data'
 
 def test_lower_50k():
-  person = Person()
+  person = Person(age=39,
+                  workclass='State-gov',
+                  fnlgt=77516,
+                  education='Bachelors',
+                  education_num=13,
+                  marital_status='Never-married',
+                  occupation='Adm-clerical',
+                  relationship='Not-in-family',
+                  race='White',
+                  sex='Male',
+                  capital_gain=2174,
+                  capital_loss=0,
+                  hours_per_week=40,
+                  native_country='United-States',
+                  salary='<=50K')
   resp = client.post('/predict_salary', data=person)
   assert resp.status_code == 200
   assert resp.json() == "<=50k"
 
 def test_greater_50k():
-  person = Person(age=39,
-                workclass='State-gov',
-                fnlgt=77516,
-                education='Bachelors',
-                education_num=13,
-                marital_status='Never-married',
-                occupation='Adm-clerical',
-                relationship='Not-in-family',
-                race='White',
-                sex='Male',
-                capital_gain=2174,
-                capital_loss=0,
-                hours_per_week=40,
-                native_country='United-States',
-                salary='<=50K')
+  person = Person(age=52,
+                  workclass='Self-emp-not-inc',
+                  fnlgt=209642,
+                  education='HS-grad',
+                  education_num=9,
+                  marital_status='Married-civ-spouse',
+                  occupation='Exec-managerial',
+                  relationship='Husband',
+                  race='White',
+                  sex='Male',
+                  capital_gain=0,
+                  capital_loss=0,
+                  hours_per_week=45,
+                  native_country='United-States',
+                  salary='>50K')
 
   resp = client.post('/predict_salary', data=person)
   assert resp.status_code == 200
