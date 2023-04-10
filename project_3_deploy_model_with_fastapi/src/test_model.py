d92ec3bd3746106fbb5bef0f862d46f2a6874454
Tobias Sunderdiek <sunderdiek@gmail.com>
2023-04-10 12:40:31 +0200

    test train model

--- project_3_deploy_model_with_fastapi/src/test_model.py
+++ project_3_deploy_model_with_fastapi/src/test_model.py
@@ -1,7 +1,15 @@
 import numpy as np
-from unittest.mock import Mock
-from ml.model import inference
+from unittest.mock import Mock, patch
+from ml.model import train_model, inference
 
+@patch('ml.model.LogisticRegression')
+def test_train_model(MockLogisticRegression):
+    lr_mock = MockLogisticRegression.return_value
+    X_mock = Mock()
+    y_mock = Mock()
+    model = train_model(X_mock, y_mock)
+    lr_mock.fit.assert_called_with(X_mock, y_mock)
+    assert model is lr_mock
 
 def test_inference():
     model_mock = Mock()
