c1a71d77051781e08b5f4f83674b2fcefeabc1a5
Tobias Sunderdiek <sunderdiek@gmail.com>
2023-04-11 12:12:14 +0200

    request formatting

--- project_3_deploy_model_with_fastapi/src/check/request_render.py
+++ project_3_deploy_model_with_fastapi/src/check/request_render.py
@@ -7,22 +7,23 @@ RENDER_APP_URL = 'https://predict-salary-fhxt.onrender.com'
 logging.basicConfig(level=logging.DEBUG)
 
 def post_to_render():
-    person = {"age":39,
-            "workclass": "State-gov",
-            "fnlgt":77516,
-            "education":"Bachelors",
-            "education-num":13,
-            "marital-status":"Never-married",
-            "occupation":"Adm-clerical",
-            "relationship":"Not-in-family",
-            "race":"White",
-            "sex":"Male",
-            "capital-gain":2174,
-            "capital-loss":0,
-            "hours-per-week":40,
-            "native-country":"United-States"}
-    logging.info(f"Post {person} to {RENDER_APP_URL}")
-    res = requests.post(RENDER_APP_URL, data=person)
+    post_path = f"{RENDER_APP_URL}/predict_salary"
+    person = "{\"age\":39,\
+            \"workclass\": \"State-gov\",\
+            \"fnlgt\":77516,\
+            \"education\":\"Bachelors\",\
+            \"education-num\":13,\
+            \"marital-status\":\"Never-married\",\
+            \"occupation\":\"Adm-clerical\",\
+            \"relationship\":\"Not-in-family\",\
+            \"race\":\"White\",\
+            \"sex\":\"Male\",\
+            \"capital-gain\":2174,\
+            \"capital-loss\":0,\
+            \"hours-per-week\":40,\
+            \"native-country\":\"United-States\"}"
+    logging.info(f"Post {person} to {post_path}")
+    res = requests.post(post_path, data=person)
     logging.info(f"Response status code: {res.status_code}")
     logging.info(f"Response msg: {res.json()}")
 
