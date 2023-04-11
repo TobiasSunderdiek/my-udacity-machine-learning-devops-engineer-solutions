e9f505d90f72a31f6244556fbf518d9c398ec12f
Tobias Sunderdiek <sunderdiek@gmail.com>
2023-04-11 14:44:07 +0200

    add example for person model

--- project_3_deploy_model_with_fastapi/src/dto/person.py
+++ project_3_deploy_model_with_fastapi/src/dto/person.py
@@ -16,3 +16,23 @@ class Person(BaseModel):
     capital_loss: int = Field(..., alias='capital-loss')
     hours_per_week: int = Field(..., alias='hours-per-week')
     native_country: str = Field(..., alias='native-country')
+
+    class Config:
+        schema_extra = {
+            "example": {
+                'age': 39,
+                'workclass': 'State-gov',
+                'fnlgt': 77516,
+                'education': 'Bachelors',
+                'education-num': 13,
+                'marital-status': 'Never-married',
+                'occupation': 'Adm-clerical',
+                'relationship': 'Not-in-family',
+                'race': 'White',
+                'sex': 'Male',
+                'capital-gain': 2174,
+                'capital-loss': 0,
+                'hours-per-week': 40,
+                'native-country': 'United-States'
+            }
+        }
